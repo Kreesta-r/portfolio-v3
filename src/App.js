0168d3056a16c6f12a1f72a3d9caec11dc29838f
Kreesta <kreesta@localhost.localdomain>
2024-05-31 14:09:00 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Projects from './components/projects/Projects'
+import Navbar from './components/navbar/Navbar.jsx'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+      <Navbar/>
+      </div>
+      <div className="wrapper">
+      <BrowserRouter>
+      <Routes>
+        <Route path = '/' element = {<Home/>}/>
+        <Route path = '/projects' element = {<Projects/>}/>
+      </Routes>
+     </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,10 @@
+import './projects.css'
+
+function Projects (){
+    return(
+        <div>
+            Projects
+        </div>
+    )
+}
+export default Projects
